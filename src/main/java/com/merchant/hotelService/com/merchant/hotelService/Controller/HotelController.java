9d894074a6a88d827dbf7bbf85ec043f1c01ee16
arteuryu <yuerte969@pingan.com.cn>
2016-08-13 02:38:34 +0800

    rm .idea

--- src/main/java/com/merchant/hotelService/com/merchant/hotelService/Controller/HotelController.java
+++ src/main/java/com/merchant/hotelService/com/merchant/hotelService/Controller/HotelController.java
@@ -1,20 +1,17 @@
 package com.merchant.hotelService.com.merchant.hotelService.Controller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Created by yuerte969 on 12/8/2016.
+ * Created by yuerte969 on 13/8/2016.
  */
 @RestController
-public class TestController {
-    @RequestMapping("/arteur")
+public class HotelController {
+    @RequestMapping("/arteuryu")
     @ResponseBody
     String home() {
-        return "Hello World!";
+        return "Hello Hotel";
     }
 }
